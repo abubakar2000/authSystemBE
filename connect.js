1da4c99943335a9074ec52ad097a2b96d5977be2
Saram Atif <92883691+saramk98@users.noreply.github.com>
2022-01-06 14:04:18 +0500

    connection code

---
+++ connect.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql')
+require('dotenv').config();
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+})
+
+connection.connect(err => {
+    let message = !err ? 'connected' : 'connection failed';
+    console.log('mysql : ${message}');
+});
+
+module.exports = connection;
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,14 @@
 var express = require('express');
+const connection = require('../connect');
 var router = express.Router();
-var server = require('./config/serverinfo.json');
+var server = require('../connect');
 
-router.get('/', (req, res) => {
-    res.send('Hello saram');
-})
-router.get('/ab/:input', (req, res) => {
-    console.log(server.host);
+router.get('/', (req, res, next) => {
     
-    res.send(`hello abubakar with ${req.params.input} ${req.query.api}`);
-})
+    connection.query('SELECT * FROM user', (err, results, fields) => {
+        !err ? res.json(results) : res.json({ err });
+    });
+});
+
 
 module.exports = router;
