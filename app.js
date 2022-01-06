b2c60fb7d6dbc5132c09887678d160515ce283f6
Saram Atif <92883691+saramk98@users.noreply.github.com>
2022-01-06 13:35:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var a;
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
